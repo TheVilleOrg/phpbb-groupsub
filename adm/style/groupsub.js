491782ea98efcbbcb68db86b9587e9221f84d2f4
Steve Guidetti <stevotvr@gmail.com>
2018-08-18 09:50:52 -0700

    Reset the term list when the form is reset

--- adm/style/groupsub.js
+++ adm/style/groupsub.js
@@ -2,26 +2,38 @@
 
 'use strict'
 
-$('.move_term_up').click(function() {
-	var row = $(this).parents('tr'),
-		prev = row.prev();
-	row.insertBefore(prev);
+var termTable = $('#term_table tbody'),
+	termHtml = termTable.html();
 
-	return false;
+var attachEvents = function() {
+	$('.move_term_up').click(function() {
+		var row = $(this).parents('tr'),
+			prev = row.prev();
+		row.insertBefore(prev);
+
+		return false;
+	});
+
+	$('.move_term_down').click(function() {
+		var row = $(this).parents('tr'),
+			next = row.next();
+		row.insertAfter(next);
+
+		return false;
+	});
+
+	$('.delete_term').click(function() {
+		$(this).parents('tr').remove();
+
+		return false;
+	});
+};
+
+$('#add_edit_pkg').on('reset', function() {
+	termTable.html(termHtml);
+	attachEvents();
 });
 
-$('.move_term_down').click(function() {
-	var row = $(this).parents('tr'),
-		next = row.next();
-	row.insertAfter(next);
-
-	return false;
-});
-
-$('.delete_term').click(function() {
-	$(this).parents('tr').remove();
-
-	return false;
-});
+attachEvents();
 
 }(jQuery));
