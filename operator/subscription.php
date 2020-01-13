5b9c0c6dbd760a5d729be8a86fe0b087713d5b2d
Steve Guidetti <stevotvr@gmail.com>
2020-01-13 14:55:38 -0800

    Fix subscription expiration dates not updating when renewing for unlimited time

--- operator/subscription.php
+++ operator/subscription.php
@@ -341,7 +341,7 @@ class subscription extends operator implements subscription_interface
 		if ($row)
 		{
 			$data = array(
-				'sub_expires'		=> (int) $row['sub_expires'] + $length,
+				'sub_expires'		=> $length > 0 ? (int) $row['sub_expires'] + $length : 0,
 				'sub_notify_status'	=> 0,
 			);
 			$sql = 'UPDATE ' . $this->sub_table . '
@@ -364,7 +364,7 @@ class subscription extends operator implements subscription_interface
 							->set_package($term->get_package())
 							->set_user((int) $user_id)
 							->set_start(time())
-							->set_expire(time() + $length);
+							->set_expire($length > 0 ? time() + $length : 0);
 		return $this->add_subscription($subscription);
 	}
 
