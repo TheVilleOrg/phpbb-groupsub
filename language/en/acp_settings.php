ba5d4b58b8333262133d09974a609adacb1b85ee
Steve Guidetti <stevotvr@gmail.com>
2021-01-22 13:02:45 -0800

    Add more detail to the API credential explanation

--- language/en/acp_settings.php
+++ language/en/acp_settings.php
@@ -47,7 +47,7 @@ $lang = array_merge($lang, array(
 	'ACP_GROUPSUB_SETTINGS_PP_SB_API'		=> 'PayPal sandbox API settings',
 	'ACP_GROUPSUB_PP_CLIENT'				=> 'Client ID',
 	'ACP_GROUPSUB_PP_SECRET'				=> 'Client secret',
-	'ACP_GROUPSUB_PP_API_EXPLAIN'			=> 'To get your <strong>Client ID</strong> and <strong>Client secret</strong>, go to the <a href="https://developer.paypal.com/developer/applications/" target="_blank">PayPal Developer Dashboard</a>. With <strong>%1$s</strong> selected at the top, click on an existing application or click <strong>Create App</strong> to create a new application. Here you will be able to access your API credentials.',
+	'ACP_GROUPSUB_PP_API_EXPLAIN'			=> 'To get your <strong>Client ID</strong> and <strong>Client secret</strong>, go to the <a href="https://developer.paypal.com/developer/applications/" target="_blank">PayPal Developer Dashboard</a>. With <strong>%1$s</strong> selected at the top, in the <strong>REST API apps</strong> section, click on an existing application or click <strong>Create App</strong> to create a new application. Here you will be able to access your API credentials.',
 	'ACP_GROUPSUB_SETTINGS_GENERAL'			=> 'General options',
 	'ACP_GROUPSUB_NOTIFY_ADMINS'			=> 'Notify admins',
 	'ACP_GROUPSUB_NOTIFY_ADMINS_EXPLAIN'	=> 'If enabled, administrators with the <em>“Can view users’ subscriptions”</em> permission will be notified of all new subscriptions.',
